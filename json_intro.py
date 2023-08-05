d8233540b61a7ca6fbe765599e13424de826794a
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-05 10:22:02 +0500

    Pro Project

--- json_intro.py
+++ json_intro.py
@@ -1,10 +1,21 @@
 #Convert json into string 
-import json
-d={
-    'course_name':'Python',
-    'fees':15000
-}
+# import json
+# d={
+#     'course_name':'Python',
+#     'fees':15000
+# }
 
-f=json.dumps(d)
-print(type(f))
-print(f)
+# f=json.dumps(d)
+# print(type(f))
+# print(f)
+
+#Convert json datato object data
+import json
+d='{    "cname":"Python","fees":12000,"duration":"2Months"    }'
+x=json.loads(d)
+print(type(x))
+# print(x)
+
+#Iterate the function
+for a in x:
+    print(a,x[a])
