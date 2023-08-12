7c4815a91dcaeb3a18b173fef7b0bc213c5edca9
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-13 03:45:19 +0500

    Pro Project

---
+++ Encapsulation.py
@@ -0,0 +1,9 @@
+class Student:
+    __name__="Ahmed"
+    def __init__(self):
+        print(self.__name__)
+        self.__displayinfo()
+    def __displayinfo(self):
+        print("Welcome to programming")
+    
+obj=Student()
