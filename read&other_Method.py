5b356f5de7b3715685068a6e783db5aa88cbd500
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-13 04:56:39 +0500

    Pro Project

--- read&other_Method.py
+++ read&other_Method.py
@@ -12,3 +12,4 @@ while True:
     print(f"Marks of Student {i}in Math is:{m1*2}")
     print(f"Marks of Student {i}in English is:{m2*2}")
     print(f"Marks of Student {i}in SST is:{m3*2}")
+    print(line)
