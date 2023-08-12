104112cf5d512170c9101b55eb414bec4237f700
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-13 02:50:43 +0500

    Pro Project

--- inheritance.py
+++ inheritance.py
@@ -1,12 +1,18 @@
 #Inheritance is two or more object is called inheritance:
-#Single line Inheritance:
+# #Single line Inheritance:
+# class A:
+#     def displayA(self):
+#         print("Welcome to programming A")
+    
+# class B(A):
+#     def displayB(self):
+#         print("Welcome to Programming B")
+# obj=B()
+# obj.displayA()
+# obj.displayB()
+
+#Multi-Level inheritance:
 class A:
     def displayA(self):
-        print("Welcome to programming A")
-    
-class B(A):
-    def displayB(self):
-        print("Welcome to Programming B")
-obj=B()
-obj.displayA()
-obj.displayB()
+        print("Welcome to Programming A")
+        
