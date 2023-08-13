78514d79d7ad7522c6b60c7e293efc1d6a1224a0
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-13 05:11:48 +0500

    Pro Project

--- read&other_Method.py
+++ read&other_Method.py
@@ -1,15 +1,15 @@
 #We will see read,readlines and other methods:
-f=open('myfile.txt','r')
-i=0
-while True:
-    i=i+1
-    line=f.readline()
-    if not line:
-        break
-    m1=line.split(",")[0]
-    m2=line.split(",")[1]
-    m3=line.split(",")[2]
-    print(f"Marks of Student {i}in Math is:{m1}")
-    print(f"Marks of Student {i}in English is:{m2}")
-    print(f"Marks of Student {i}in SST is:{m3}")
-    print(line)
+# f=open('myfile.txt','r')
+# i=0
+# while True:
+#     i=i+1
+#     line=f.readline()
+#     if not line:
+#         break
+#     m1=line.split(",")[0]
+#     m2=line.split(",")[1]
+#     m3=line.split(",")[2]
+#     print(f"Marks of Student {i}in Math is:{m1}")
+#     print(f"Marks of Student {i}in English is:{m2}")
+#     print(f"Marks of Student {i}in SST is:{m3}")
+#     print(line)
