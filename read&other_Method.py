d6c87cbb584e0f1986b51e1f96000a7620903620
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-13 05:02:41 +0500

    Pro Project

--- read&other_Method.py
+++ read&other_Method.py
@@ -6,10 +6,10 @@ while True:
     line=f.readline()
     if not line:
         break
-    m1=int(line.split(",")[0])
-    m2=int(line.split(",")[1])
-    m3=int(line.split(",")[2])
-    print(f"Marks of Student {i}in Math is:{m1*2}")
-    print(f"Marks of Student {i}in English is:{m2*2}")
-    print(f"Marks of Student {i}in SST is:{m3*2}")
+    m1=line.split(",")[0]
+    m2=line.split(",")[1]
+    m3=line.split(",")[2]
+    print(f"Marks of Student {i}in Math is:{m1}")
+    print(f"Marks of Student {i}in English is:{m2}")
+    print(f"Marks of Student {i}in SST is:{m3}")
     print(line)
