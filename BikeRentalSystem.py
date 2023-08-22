4591e4696a30cca0e81e6a6a57da14b3ff42b5f2
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-23 03:13:28 +0500

    Pro Project

--- BikeRentalSystem.py
+++ BikeRentalSystem.py
@@ -1,3 +1,16 @@
 #We are making Bike Ranking System:
 class bikeshop:
-    
+    def __init__ (self,stock):
+        self.stock = stock
+    def displayinfoBike(self):
+        print("Total Bikes",self.stock)
+    def rentForBike(self,q):
+        if q<0:
+            print("Enter the positive value or greater than zero")
+        elif q>self.stock:
+            print("Enter the vlaue (less than stack)")
+        else:
+            self.stock=self.stock-q
+            print("Total Prices",q*100)
+            print("Total Bikes",self.stock)
+while
