456bba1222706f02cdcb42c948c1eaa5c752f7ba
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-22 14:02:43 +0500

    Pro Project

---
+++ Method_overlod_override.py
@@ -0,0 +1,14 @@
+#overloading example:
+class Area:
+    def find_area(self,x=None,y=None):
+        if x!=None and y!=None:
+            print("Area of rectangle:",(x*y))
+        elif x!=None:
+            print("Area of square:",(x*x))
+        else:
+            print("Nothing")
+
+obj1=Area()
+obj1.find_area()
+obj1.find_area(10)
+obj1.find_area(10,20)
