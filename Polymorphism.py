70ccb4d671e90ed0a62d0b346a2a09c39686bc6a
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-22 13:39:16 +0500

    Pro Project

--- Polymorphism.py
+++ Polymorphism.py
@@ -1,3 +1,13 @@
 #In this code we are doing polymorphism
 l=[10,20,30,40]
 print(len(l))
+s = "Welcome"
+print(len(s))
+
+#Overloading example:
+class Ws:
+    def displayinfo(self,name=''):
+        print("Welcome to coding:"+name)
+obj=Ws()
+obj.displayinfo()
+obj.displayinfo(" Python")
