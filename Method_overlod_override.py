f3d718fadb873e8cfd8e15dba4749141ed571f65
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-22 17:30:56 +0500

    Pro Project

--- Method_overlod_override.py
+++ Method_overlod_override.py
@@ -14,3 +14,12 @@ obj1.find_area(10)
 obj1.find_area(10,20)
 
 #Method overriding example:
+class A:
+    def showData(self):
+        print("I am in A class")
+
+class B(A):
+    def showData(self):
+        print("I am in B class")
+obj=B()
+obj.showData()
