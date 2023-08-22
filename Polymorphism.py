25c69158c359d37c1633a8866e93c785e0ffa7f8
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-22 13:47:24 +0500

    Pro Project

--- Polymorphism.py
+++ Polymorphism.py
@@ -1,13 +1,26 @@
-#In this code we are doing polymorphism
+#In this code we are doing polymorphism(Same function name but different signature and being uses for different types:)
 l=[10,20,30,40]
 print(len(l))
 s = "Welcome"
 print(len(s))
 
-#Overloading example:
+#Overloading example:(function name same but different parameters)
+# class Ws:
+#     def displayinfo(self,name=''):
+#         print("Welcome to coding:"+name)
+# obj=Ws()
+# obj.displayinfo()
+# obj.displayinfo(" Python")
+
+#Overriding example:(same function name but different class is called overriding)
 class Ws:
-    def displayinfo(self,name=''):
-        print("Welcome to coding:"+name)
-obj=Ws()
+    def displayinfo(self):
+        print("Welcome to Python ")
+
+class IIP(Ws):
+    def displayinfo(self):
+        super().displayinfo()
+        print("Welcome to IIp")
+
+obj=IIP()
 obj.displayinfo()
-obj.displayinfo(" Python")
