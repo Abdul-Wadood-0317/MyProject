87661a02405d20bd2e3e6bd8752004622dbf97c0
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-28 22:56:04 +0500

    Add more Code

---
+++ LambdaFunc.py
@@ -0,0 +1,10 @@
+#Lambda Function = Anonymous Function
+def appl(fx,value):
+    return 6+fx(value)
+    double = lambda x:x*2
+    cube = lambda x:x*x*x
+    avg = lambda x,y,z:(x+y+z)/3
+print(double(5))
+print(cube(5))
+print(avg(5))
+print(appl(lambda x:x*x,2))
