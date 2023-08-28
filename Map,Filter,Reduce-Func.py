98c355dafa932d6e46ffdd6b113b3458197bfc31
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-29 03:05:22 +0500

    Add more Code

--- Map,Filter,Reduce-Func.py
+++ Map,Filter,Reduce-Func.py
@@ -1,5 +1,20 @@
 #We will see other functions in Python
 #MAP
-def cub(x):
-    return x*x*x
-print(cub(3))
+# def cub(x):
+#     return x*x*x
+# print(cub(3))
+
+l = [1,2,4,6,4,3]
+# newl = []
+# for item in l:
+#     newl.append(cub(item))
+
+newl = list(map(lambda x:x*x*x,l))
+print(newl)
+
+#Filter 
+def filter_function (a):
+    return a>2
+
+newl = list(filter(filter_function,l))
+print(newl)
