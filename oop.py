16ed2784700de1d2ca0ff4f0a87efaed0f928ec2
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-29 10:19:33 +0500

    Add more Code

--- oop.py
+++ oop.py
@@ -1,11 +1,24 @@
 #Now we movig towards Object Oriented
-class DemoClass:
-    a =10
-    def sumvalue(self):
-        print(20+30)
+# class DemoClass:
+#     a =10
+#     def sumvalue(self):
+#         print(20+30)
 
-demoobject=DemoClass()
-demoobject1=DemoClass()
-print(demoobject.a)
-print(demoobject1.a)
-demoobject.sumvalue()
+# demoobject=DemoClass()
+# demoobject1=DemoClass()
+# print(demoobject.a)
+# print(demoobject1.a)
+# demoobject.sumvalue()
+
+#Encapsulation
+#Polymorphism
+#Inheritance
+
+class Person:
+    name = "Ahmed"
+    occupation = 'Software Developer'
+    networth = 10
+    def info (self):
+        print(f"{self.name}is a {self.occupation}")
+obj =Person()
+
