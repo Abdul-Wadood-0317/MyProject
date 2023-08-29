430adca8c2bf89a742c69f74fd941ae5a799fb73
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-29 12:24:43 +0500

    Add more Code

--- Decorators.py
+++ Decorators.py
@@ -1,17 +1,17 @@
-# def greet(fx):
-#     def mfx(*args,**kwargs):
-#         print("Good Morning!")
-#         fx(*args,**kwargs)
-#         print("Thanks for using this function")
-#     return mfx
+def greet(fx):
+    def mfx(*args,**kwargs):
+        print("Good Morning!")
+        fx(*args,**kwargs)
+        print("Thanks for using this function")
+    return mfx
 
-# @greet
-# def hello():
-#     print("Hello World!")
+@greet
+def hello():
+    print("Hello World!")
 
-# @greet
-# def add(a,b):
-#     print(a+b)
+@greet
+def add(a,b):
+    print(a+b)
 
-# hello()
-# add(1,2)
+hello()
+add(1,2)
