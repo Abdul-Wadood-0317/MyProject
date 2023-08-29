3e05147ba8f1166a552bbed13712afda2bd919fd
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-29 10:07:31 +0500

    Add more Code

---
+++ is VS ==.py
@@ -0,0 +1,11 @@
+#The difference between these two operators
+#key points:
+#is basically used for mention the location
+#== is basically an compare value operator
+a = [1,2,3]
+b = [1,2,4]
+print( a is b)  #exact location of object 
+# print(a is None)
+print( a == b)  #it gives values 
+
+
