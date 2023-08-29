07151537011b70798afcfe72c8cca92f558797fb
Abdul Wadood <abdulwadood0317@gmail.com>
2023-08-29 13:12:29 +0500

    Add more Code

--- Geter_Seter_Method.py
+++ Geter_Seter_Method.py
@@ -1,12 +1,12 @@
-class Student:
-    def __init__(self) :
-        self.__name__=""
-    def getname(self):
-        return self.__name__
-    def setname(self,name):
-        self.__name__=name
-obj=Student()
-obj.setname("Testing")
-name=obj.getname()
-print(name)
-#Keep doing and Hardwork,Never give up!
+# class Student:
+#     def __init__(self) :
+#         self.__name__=""
+#     def getname(self):
+#         return self.__name__
+#     def setname(self,name):
+#         self.__name__=name
+# obj=Student()
+# obj.setname("Testing")
+# name=obj.getname()
+# print(name)
+# #Keep doing and Hardwork,Never give up!
