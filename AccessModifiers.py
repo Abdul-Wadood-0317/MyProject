f441eb30e4303123552b967b04ea6a152c4fe5fc
Abdul Wadood <abdulwadood0317@gmail.com>
2023-09-20 10:25:58 +0500

    first commit

---
+++ AccessModifiers.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self) :
+        self._name="Abdul"
+    def _funName(self):     #protected method
+        return "CodeWithWadood"
+
+class Subject(Student):     #inherited class
