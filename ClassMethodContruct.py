e8bd88e0933f6c61b3524b0410baef1a44f05475
Abdul Wadood <abdulwadood0317@gmail.com>
2023-10-15 03:06:03 +0500

    first commit

--- ClassMethodContruct.py
+++ ClassMethodContruct.py
@@ -14,3 +14,18 @@ string = "John-12000"
 e2 = Employee.fromStr(string)
 print(e2.name)
 print(e2.salary)
+
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+    @classmethod
+    def from_string(cls,string):
+        name,age = string.split(",")
+        return cls(name,int(age))
+
+person =Person.from_string("JohnDoe,30")
+print(person.name,person.age)
+
+
