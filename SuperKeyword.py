7740926658416cc105798399d9ebce97838e38a9
Abdul Wadood <abdulwadood0317@gmail.com>
2023-10-15 23:09:59 +0500

    first commit

---
+++ SuperKeyword.py
@@ -0,0 +1,14 @@
+#Mainly Super Keyword is used child class method in to parent class method and to call them 
+class Employee:
+    def __init__ (self,name,id):
+        self.name = name
+        self.id = id
+class Programmer(Employee):
+    def __init__(self,name,id,lang):
+        super().__init__(name,id)
+        self.lang = lang
+rohan =Employee("AbdulWadood","760")
+#waleed = Programmer("Asad","2345","Python")
+print(rohan.name)
+print(rohan.id)
+print(rohan.lang)
