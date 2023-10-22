69e03018e9ac4b38ce7369f0f4283d413dbcede2
Abdul Wadood <abdulwadood0317@gmail.com>
2023-10-22 10:13:52 +0500

    first commit

--- MagicMethod.py
+++ MagicMethod.py
@@ -2,3 +2,30 @@
 # start and end with the double underscores.
 
 #code starts from here
+#1)__init__ method:
+class Employee:
+    name = "Harry"
+    def __len__(self):
+        i=0
+        for c in self.name:
+            i=i+1
+        return i
+e=Employee()
+print(e.name)
+print(len(e.name))
+
+#__str__ and __rep__ method:
+class Employee:
+    def __init__ (self, name):
+        self.name = name
+    def __len__(self):
+        i= 0 
+        for c in self.name:
+            i = i+1
+        return i
+    def __str__(self):
+        return f"The name of employee is {self.name} str"
+    
+    def __repr__(self):
+        return f"Employee('{self.name}')"
+        
