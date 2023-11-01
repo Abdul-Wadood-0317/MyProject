9b607fa72cae8ef1d293193de81cf9ae1f3c64ba
Abdul Wadood <abdulwadood0317@gmail.com>
2023-11-02 01:19:03 +0500

    first commit

--- Hackerrank.py
+++ Hackerrank.py
@@ -1,13 +1,22 @@
-import math
-import os
-import random
-import re
-import sys
+# Problem #1
+# import math
+# import os
+# import random
+# import re
+# import sys
 
+# if __name__ == '__main__':
+#     n = int(input().strip())
+
+# if (n%2!=0)or (n>=6 and n<=20):
+#     print("Not Weird")
+# else:
+#     print("Weird")
+
+#Problem no 2
 if __name__ == '__main__':
-    n = int(input().strip())
-
-if (n%2!=0)or (n>=6 and n<=20):
-    print("Not Weird")
-else:
-    print("Weird")
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
