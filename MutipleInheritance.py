ee18d0d4e486c2040df14f00c1e70b5f9d5de9a5
Abdul Wadood <abdulwadood0317@gmail.com>
2023-11-22 18:18:40 +0500

    first commit

---
+++ MutipleInheritance.py
@@ -0,0 +1,6 @@
+#in this lecture we will see the syntax of multiple inheritance:
+#class ChildClass(ParentClass1,ParentClass2,ParentClass3):
+class Employee:
+    def __init__(self,name):
+        self.name = name
+        
