770ce1306dff6a026c232a5107178aa0e7558872
Abdul Wadood <abdulwadood0317@gmail.com>
2023-11-22 12:29:58 +0500

    first commit

--- SingleInheritance.py
+++ SingleInheritance.py
@@ -3,3 +3,18 @@ class Animal:
     def __init__(self,name,species):
         self.name = name
         self.species = species
+    
+    def make_sound(self):
+        print(f"sound made by the animal")
+
+class Dog(Animal):
+    def __init__(self,name,breed):
+        Animal.__init__(self,name,species = "Dog")
+        self.breed = breed
+    def make_sound(self):
+        print("Bark!")
+
+d = Dog("Dog","Dogerman")
+d.make_sound()
+a = Animal("Dog","Dog")
+a.make_sound()
