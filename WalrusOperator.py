29a8a5d17a7dd7370cd1735aacd0a5592ae6cdae
Abdul Wadood <abdulwadood0317@gmail.com>
2023-11-26 19:07:51 +0500

    first commit

--- WalrusOperator.py
+++ WalrusOperator.py
@@ -2,10 +2,13 @@ numbers = [1,2,3,4,5]
 while (n:=len(numbers))>0:
     print(numbers.pop())
 #Aam Zindagi:
+# foods = list()
+# while True:
+#     food = input("What food do you like?:")
+#     if food == "quit":
+#             break
+#     foods.append(food)
+#Mentos Zindagi:
 foods = list()
-while True:
-    food = input("What food do you like?:")
-    if food == "quit":
-            break
+while (food := input ("What food do you like?:")) !="quit":
     foods.append(food)
-#Mentos Zindagi
