4133fb3c0460461a19e64de69e28db25d518f3e3
Abdul Wadood <abdulwadood0317@gmail.com>
2023-11-28 04:06:53 +0500

    first commit

--- RequestModule.py
+++ RequestModule.py
@@ -1,5 +1,7 @@
 # from urllib import requests
 from pip._vendor import requests
 import pip._vendor.requests
+
+from bs4 import BeautifulSoup
 response = requests.get("https://www.wikipedia.com")
 print(response.text)
