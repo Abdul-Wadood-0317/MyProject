8f4cafb43bfb6f884c153a5e01d64fdd3c950161
Abdul Wadood <abdulwadood0317@gmail.com>
2023-11-28 23:57:32 +0500

    first commit

--- RequestModule.py
+++ RequestModule.py
@@ -1,7 +1,13 @@
 # from urllib import requests
 from pip._vendor import requests
+from bs4 import BeautifulSoup
 import pip._vendor.requests
 
-from bs4 import BeautifulSoup
-response = requests.get("https://www.wikipedia.com")
-print(response.text)
+url = "www.geekforgeeks.com"
+r = requests.get(url)
+print(r.text)
+soup =BeautifulSoup(r.text,"html.parser")
+print(soup.prettify())
+# response = requests.get("https://www.wikipedia.com")
+# print(response.text)
+#code krna parega bhai
