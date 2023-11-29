439788be7f7fa60a14ac6fb042edfc12eb00168a
Abdul Wadood <abdulwadood0317@gmail.com>
2023-11-30 02:52:01 +0500

    first commit

---
+++ Generators.py
@@ -0,0 +1,9 @@
+#It is used for special  of function that allow you to create an iterable sequence of values.
+def my_generator():
+    for i in range(500):
+        #complex computation
+        yield i
+gen = my_generator()
+
+for j in gen:
+    print(j)
