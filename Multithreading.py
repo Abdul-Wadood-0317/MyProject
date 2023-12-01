db9c39db0f8135f05db746877b0e09432fd53706
Abdul Wadood <abdulwadood0317@gmail.com>
2023-12-01 12:44:20 +0500

    first commit

--- Multithreading.py
+++ Multithreading.py
@@ -4,3 +4,14 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 
 #Indicates some task being done
+def func (seconds):
+    print(f"Sleeping for {seconds} seconds")
+    time.sleep(seconds)
+    return seconds
+def main():
+    time1 = time.perf_counter()
+    func(4)
+    func(2)
+    func(1)
+
+    #Smae code using thread
