13c03cbd71581fd46c3d66f91f9a598b2fad1351
Abdul Wadood <abdulwadood0317@gmail.com>
2023-12-02 01:02:58 +0500

    first commit

--- Multithreading.py
+++ Multithreading.py
@@ -41,6 +41,6 @@ def poolingDemo():
         #print(future2.result())
         #print(future3.result())
         l = [3,5,1,2]
-        results = executor.mp(func,l)
-        for result in result:
+        results = executor.map(func,l)
+        for result in results:
             print(result)
