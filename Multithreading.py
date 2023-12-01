6566a1aede9d2595b2e055e4804a907b42b7ae9a
Abdul Wadood <abdulwadood0317@gmail.com>
2023-12-02 00:55:08 +0500

    first commit

--- Multithreading.py
+++ Multithreading.py
@@ -8,29 +8,29 @@ def func (seconds):
     print(f"Sleeping for {seconds} seconds")
     time.sleep(seconds)
     return seconds
+def main():
+    time1 = time.perf_counter()
+        # func(4)
+        # func(2)
+        # func(1)
 
-time1 = time.perf_counter()
-    # func(4)
-    # func(2)
-    # func(1)
-
-    #Same code using thread
-t1 = threading.Thread(target = func,args=[4])
-t2 = threading.Thread(target = func,args=[2])
-t3 = threading.Thread(target = func,args=[1])
+        #Same code using thread
+    t1 = threading.Thread(target = func,args=[4])
+    t2 = threading.Thread(target = func,args=[2])
+    t3 = threading.Thread(target = func,args=[1])
 
 
-t1.start()
-t2.start()
-t3.start()
+    t1.start()
+    t2.start()
+    t3.start()
 
-t1.join()
-t2.join()
-t3.join()
+    t1.join()
+    t2.join()
+    t3.join()
 
-#Calculating Time
-time2 = time.perf_counter()
-print(time2-time1)
+    #Calculating Time
+    time2 = time.perf_counter()
+    print(time2-time1)
 
 def poolingDemo():
     with ThreadPoolExecutor() as executor:
@@ -40,4 +40,7 @@ def poolingDemo():
         #print(future1.result())
         #print(future2.result())
         #print(future3.result())
-        l = []
+        l = [3,5,1,2]
+        results = executor.mp(func,l)
+        for result in result:
+            print(result)
