a9444a56e957faf91d789cf00e2aac09d829078c
Abdul Wadood <abdulwadood0317@gmail.com>
2023-12-02 00:20:01 +0500

    first commit

--- Multithreading.py
+++ Multithreading.py
@@ -14,5 +14,17 @@ def main():
     func(2)
     func(1)
 
-    #Smae code using thread
-    #programming is usually taught by examples
+    #Same code using thread
+t1 = threading.Thread(target = func,args=[4])
+t2 = threading.Thread(traget = func,args=[2])
+t3 = threading.Thread(target = func,args=[1])
+
+t1.start()
+t2.start()
+t3.start()
+
+t1.join()
+t2.join()
+t3.join()
+
+    
