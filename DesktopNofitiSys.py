5c2fb1e8a2fb956a5daa74542fef8d363ab2097b
Abdul Wadood <abdulwadood0317@gmail.com>
2023-12-02 11:07:07 +0500

    first commit

---
+++ DesktopNofitiSys.py
@@ -0,0 +1,10 @@
+#Source Code for Desktop Notification System:
+from plyer import notification 
+import time
+
+#Setting up Notification:
+notification_title = "Notification Title"
+notification_message = "Drink Water"
+
+notification_timeout = 5 #in seconds
+#Display
