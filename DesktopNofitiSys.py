8433cb6a469da170a44d3518e71535874c05fc76
Abdul Wadood <abdulwadood0317@gmail.com>
2023-12-04 23:52:38 +0500

    first commit

--- DesktopNofitiSys.py
+++ DesktopNofitiSys.py
@@ -14,4 +14,4 @@ for i in range(5):
         message = notification_message,
         timeout = notification_timeout
         )
-#i have to make my project for job and 
+#i have to make my project for job 
